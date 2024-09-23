52842d414dce85b8994703c63cfc1f2c5799d34b
Simon <bosen.xu@mail.utoronto.ca>
2024-09-23 10:39:14 -0400

    Cleaned up formatting issues in CountryCodeConverter.java

--- src/main/java/org/translation/CountryCodeConverter.java
+++ src/main/java/org/translation/CountryCodeConverter.java
@@ -6,7 +6,6 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 /**
  * This class provides the service of converting country codes to their names.
